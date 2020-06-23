608f57f97f209a42c79601791eacbf7687fd2eb4
Muhammed Misir <muhammed.misir@googlemail.com>
2020-06-23 23:31:43 +0200

    initial commit

---
+++ image-service/src/main/java/com/avocado/apps/imageservice/ImageController.java
@@ -0,0 +1,22 @@
+package com.avocado.apps.imageservice;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController()
+@RequestMapping("/api")
+public class ImageController {
+
+    @GetMapping(value = "/images", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Image> getImages() {
+        return Arrays.asList(
+                new Image(1, "Treehouse of Horror V", "https://www.imdb.com/title/tt0096697/mediaviewer/rm3842005760"),
+                new Image(2, "The Town", "https://www.imdb.com/title/tt0096697/mediaviewer/rm3698134272"),
+                new Image(3, "The Last Traction Hero", "https://www.imdb.com/title/tt0096697/mediaviewer/rm1445594112"));
+    }
+}
---
+++ image-service/src/test/java/com/avocado/apps/imageservice/ImageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avocado.apps.imageservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
